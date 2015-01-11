0dd85d61e79c1696eb5f9b62478eac4686fcccff
Roger Keller <rogerk@unifysquare.com>
2015-01-11 17:35:35 +0100

    Update to build against .Net 4.0 and delay-sign during build.

---
+++ tools/build.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+set nuget=%~dp0\NuGet\NuGet.exe
+set src=%~dp0\..\src
+
+pushd %src%\Lucene.Net.ObjectMapping
+
+msbuild /p:Configuration=Release;Platform=AnyCPU /m Lucene.Net.ObjectMapping.csproj /t:Clean,Build
+sn -R bin\Release\Lucene.Net.ObjectMapping.dll %KEYFILE%
+
+%nuget% pack Lucene.Net.ObjectMapping.csproj -Prop Configuration=Release -Symbols -IncludeReferencedProjects
+
+popd
