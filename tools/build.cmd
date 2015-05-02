9b163505106cd3f7300a9caf241d9a2213728e55
Roger Keller <rokeller@cymbeline.ch>
2015-05-02 14:22:23 +0200

    Prepare for 1.1.0-alpha release.

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,11 +1,21 @@
 @echo off
 
+if "%KEYFILE%" == "" (
+    echo Error: The key file is not specified.
+    goto :EOF
+)
+
 set nuget=%~dp0\NuGet\NuGet.exe
 set src=%~dp0\..\src
 
 pushd %src%\Lucene.Net.ObjectMapping
 
 msbuild /p:Configuration=Release;Platform=AnyCPU /m Lucene.Net.ObjectMapping.csproj /t:Clean,Build
+
+echo Sign the output assembly ...
 sn -R bin\Release\Lucene.Net.ObjectMapping.dll %KEYFILE%
+
+echo Verify that the output assembly was signed ...
+sn -v bin\Release\Lucene.Net.ObjectMapping.dll
 
 popd
