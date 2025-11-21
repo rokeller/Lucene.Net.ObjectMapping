7e417d12ae019511d2dc1bafd465bdc344116aec
Roger Keller <rokeller@users.noreply.github.com>
2025-11-21 12:09:05 +0100

    Add support for net10 (#18)

--- examples/DirectoryWeb/Models/Person.cs
+++ examples/DirectoryWeb/Models/Person.cs
@@ -3,8 +3,8 @@ namespace DirectoryWeb.Models;
 public class Person
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
 
     public int YearOfBirth { get; set; }
 }
--- examples/DirectoryWeb/Models/SearchRequest.cs
+++ examples/DirectoryWeb/Models/SearchRequest.cs
@@ -2,5 +2,5 @@ namespace DirectoryWeb.Models;
 
 public sealed class SearchRequest
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 }
